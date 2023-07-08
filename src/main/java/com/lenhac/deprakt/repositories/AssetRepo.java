a9ed79327eaa0bc8340bf5e9c096cd080775fb02
LeeN <leendegwa@gmail.com>
2023-07-08 07:22:36 +0300

    start of core java

---
+++ src/main/java/com/lenhac/deprakt/repositories/AssetRepo.java
@@ -0,0 +1,2 @@
+package com.lenhac.deprakt.repositories;public interface AssetRepo {
+}
---
+++ src/test/java/com/lenhac/deprakt/models/AssetTest.java
@@ -0,0 +1,11 @@
+package com.lenhac.deprakt.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AssetTest {
+
+    @Test
+    void testToString() {
+    }
+}
