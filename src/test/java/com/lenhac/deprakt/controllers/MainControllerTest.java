20a5266de15087bac52f337a276eeb8310d73954
LeeN <leendegwa@gmail.com>
2023-07-10 21:34:09 +0300

    Functioning /asset submission

---
+++ src/test/java/com/lenhac/deprakt/controllers/MainControllerTest.java
@@ -0,0 +1,11 @@
+package com.lenhac.deprakt.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MainControllerTest {
+
+    @Test
+    void createAsset() {
+    }
+}
