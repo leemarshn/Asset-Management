a30ff1373060ee1a6021d989626861d3342809dd
LeeN <leendegwa@gmail.com>
2023-07-25 08:33:55 +0300

    v2: handling user AUthentication

---
+++ src/main/java/com/lenhac/deprakt/repositories/RolesRepository.java
@@ -0,0 +1,21 @@
+package com.lenhac.deprakt.repositories;
+
+import com.lenhac.deprakt.models.Permission;
+import com.lenhac.deprakt.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Role, Long> {
+
+    // Custom method to find a role by its name
+    Role findByRoleName(String roleName);
+
+    // Custom method to find permissions by their names associated with a role
+    @Query("SELECT p FROM Role r JOIN r.permissions p WHERE r.roleName = :roleName")
+    List<Permission> findPermissionsByRoleName(@Param("roleName") String roleName);
+}
